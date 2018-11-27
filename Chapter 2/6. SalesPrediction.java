2226cfdc5b8acf1c3ed33144a81695460d53cf22
Kal15 <34330990+Kal15@users.noreply.github.com>
2018-11-27 00:39:30 -0800

    Create 6. SalesPrediction.java

---
+++ Chapter 2/6. SalesPrediction.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgrammingChallenges;
+/**
+ *
+ * @author K
+ */
+public class SalesPrediction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int totalSales = 4600000;
+        double sales = 0.62 * totalSales;
+    }
+    
+}
