a61e95cdaa95a3a2dadbaa40bbc3db1aa55c9140
Kal15 <34330990+Kal15@users.noreply.github.com>
2018-11-27 00:23:19 -0800

    Create 2. NameAndInitials.java

---
+++ Chapter 2/2. NameAndInitials.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgrammingChallenges;
+/**
+ *
+ * @author K
+ */
+public class NameAndInitials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String firstName = "Joe", middleName = "Doe", lastName = "Mahoney";
+        char firstInitial = 'J', middleInitial = 'D', lastInitial = 'M';
+        
+        System.out.println(firstName + " " + middleName + " " + lastName);
+        System.out.println(firstInitial + " " + middleInitial + " " + lastInitial);
+    }
+    
+}
