bd11cc999f4fcdad03a30f52304011821af404a4
Kal15 <34330990+Kal15@users.noreply.github.com>
2018-11-27 00:27:22 -0800

    Create 3. PersonalInformation.java

---
+++ Chapter 2/3. PersonalInformation.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgrammingChallenges;
+/**
+ *
+ * @author K
+ */
+public class PersonalInformation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Joe Mahoney\n"
+                + "12345 1st Ave SW CandyLand, ZZ 10301\n"
+                + "012-345-6789\n"
+                + "Computer Science");
+    }
+    
+}
