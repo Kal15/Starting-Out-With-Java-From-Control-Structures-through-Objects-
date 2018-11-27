2f2cc4fb1c202fe46ff37d83e5ae781f3766adae
Kal15 <34330990+Kal15@users.noreply.github.com>
2018-11-27 00:36:11 -0800

    Create 5. SumOfTwoNumbers.java

---
+++ Chapter 2/5. SumOfTwoNumbers.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgrammingChallenges;
+/**
+ *
+ * @author K
+ */
+public class SumOfTwoNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int var1 = 62, var2 = 99;
+        int total = var1 + var2;
+    }
+    
+}
